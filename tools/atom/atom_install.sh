a7325a5e8d94a9add01577ea8ad91632373ea0d3
maning <maning@maning.(none)>
2016-04-22 21:07:45 +0800

    [tools]在ubuntu上安装和卸载atom

---
+++ tools/atom/atom_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+#install atom on ubuntu 14.04
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
---
+++ tools/atom/atom_uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+#uninstall atom on ubuntu 14.04
+sudo apt-get remove atom
+sudo add-apt-repository --remove ppa:webupd8team/atom
