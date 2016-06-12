0b7e16651d968dea8f2951b562f6abaef1dff212
maning <maning@maning.none>
2016-06-12 20:21:19 +0800

    [tools] wiznode install

---
+++ tools/wiznode/wiznode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# install wiznote
+#
+# The binary name is WizNote. Please use WizNote to start program in cmd.
+
+sudo add-apt-repository ppa:wiznote-team
+sudo apt-get update
+sudo apt-get install wiznote
+
