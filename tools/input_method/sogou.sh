960bf3745218fd9fc2a6b1dd5aa4bc9184af995a
maning <maning@maning.none>
2016-06-13 10:56:59 +0800

    [tools] input method(搜狗输入法配置)

---
+++ tools/input_method/sogou.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# sogou pinyin install
+#
+
+# ubuntu12.04 install method
+
+sudo add-apt-repository ppa:fcitx-team/nightly
+sudo apt-get update
+
+sudo apt-get install fcitx
+
+# set keyboard input method system 
+sudo apt-get install im-switch
+im-switch -s fcitx -z default
+
+# 从官网下载sogou安装包，双击安装
+# 右键点击右上角键盘图标，点击"Input Method Configuration"->"Addon"
+# 勾选Advande-> 点击Configure
