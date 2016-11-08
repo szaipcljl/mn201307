0ec2df867fde65c3922acb684abd2dfb19c650af
nma1x <maning1x@163.com>
2016-11-08 10:46:22 +0800

    get_cur_time

--- c_code/get_cur_time.c
+++ c_code/get_cur_time.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 
+#define ct_size 20
 /******************************************************************************
  * 函数功能: 获得当前系统时间
- * 输入: szCurTimr: 结果缓冲地址
+ * 输入: szCurTimr: 字串型时间结果缓冲地址
  * 		 iFormat:    时间类型格式
- * 返回值r: 返回字串型时间的地址
+ * 返回值:
  *******************************************************************************/
-char *GetCurrentTime(char *szCurTime, int iFormat)
+int GetCurrentTime(char *szCurTime, int bufsize, int iFormat)
 {
 	time_t  tTime;
 	struct tm *localTime;
@@ -16,22 +17,22 @@ char *GetCurrentTime(char *szCurTime, int iFormat)
 	localTime=localtime(&tTime);
 
 	if (iFormat == 1) {
-		snprintf(szCurTime,100,"%d%02d%02d",localTime->tm_year+1900,
+		snprintf(szCurTime, bufsize,"%d%02d%02d",localTime->tm_year+1900,
 				localTime->tm_mon+1,localTime->tm_mday);
 	} else {
-		snprintf(szCurTime,100,"%d-%02d-%02d %02d:%02d:%02d",localTime->tm_year+1900,
+		snprintf(szCurTime, bufsize,"%d-%02d-%02d %02d:%02d:%02d",localTime->tm_year+1900,
 				localTime->tm_mon+1,localTime->tm_mday,localTime->tm_hour,localTime->tm_min,localTime->tm_sec);
 	}
 
-	return(szCurTime);
-} 
+	return 0;
+}
 
 int main(int argc, const char *argv[])
 {
 	int iFormat = 0;
-	char szCurTime[100];
+	char szCurTime[ct_size];
 
-	GetCurrentTime(szCurTime, iFormat);
+	GetCurrentTime(szCurTime, sizeof(szCurTime), iFormat);
 
 	printf("the current time: %s\n",szCurTime);
 	return 0;
