1e9e8f676bb9a4f920bd6752e4742c9443f2ef0b
maning <maning1x@163.com>
2016-11-24 20:53:03 +0800

    Makefile in different directories

---
+++ makefile/test1/f1/f1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void print1()
+{  
+	printf("Message f1.c\n");
+	return;
+}
---
+++ makefile/test1/main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../include/f1.h"
+#include "../include/f2.h"
+int main()
+{  
+	print1();
+	print2();
+
+	return 0;
+}
