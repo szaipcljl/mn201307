1a8309b4b5d896644777761fdce7d55c58f2811f
nma1x <ningx.ma@intel.com>
2017-07-12 13:13:28 +0800

    syzkaller

--- open_src/syzkaller/install_syzkaller.sh
+++ open_src/syzkaller/install_syzkaller.sh
@@ -5,9 +5,9 @@
 #Install Go 1.8.1:
 #
 # download Go distribution and unpack Go into a directory
-sudo mkdir /go /gopath
+sudo mkdir /gopath
 wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-sudo tar -zxvf go1.8.3.linux-amd64.tar.gz -C /go
+sudo tar -zxvf go1.8.3.linux-amd64.tar.gz -C /
 
 # set env var
 export GOROOT=/go
