0eca0b3cace2b8b46bab04a782d8b352b493779d
nma1x <ningx.ma@intel.com>
2017-08-17 17:49:02 +0800

    [sanitize] examples

---
+++ debug/sanitize/heap-use-after-free.cc
@@ -0,0 +1,7 @@
+//To compile: clang++ -O -g -fsanitize=address heap-use-after-free.cc
+int main(int argc, char **argv)
+{
+	int *array = new int[100];
+	delete [] array;
+	return array[argc];  // BOOM
+}
---
+++ debug/sanitize/stack-buffer-overflow.cc
@@ -0,0 +1,7 @@
+// RUN: clang -O -g -fsanitize=address %t && ./a.out
+int main(int argc, char **argv)
+{
+	int stack_array[100];
+	stack_array[1] = 0;
+	return stack_array[argc + 100];  // BOOM
+}
