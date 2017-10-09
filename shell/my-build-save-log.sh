69e462a5811225a0bbcea623f365c5c7ce7234cc
nma1x <ningx.ma@intel.com>
2017-10-09 13:38:27 +0800

    [shell] error log save

--- shell/my-build-save-log.sh
+++ shell/my-build-save-log.sh
@@ -16,7 +16,7 @@ logdir=./log_tmp
 prefix=$(find "$logdir" -type f | wc -l)
 logpath=$logdir/$prefix-$keywords$suffix.log
 
-#sudo python build.py --toolchain x86_64 | tee $logpath
+#sudo python build.py --toolchain x86_64 2>&1 | tee $logpath
 touch $logpath
 echo -n "end time: "
 date +%F-%T
