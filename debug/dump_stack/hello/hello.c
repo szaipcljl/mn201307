64205bad99a3bcf63c82d847a10147017ef556c8
nma1x <ningx.ma@intel.com>
2017-11-10 16:46:07 +0800

    update

--- debug/dump_stack/hello/hello.c
+++ debug/dump_stack/hello/hello.c
@@ -8,14 +8,17 @@
 #include <asm/traps.h>
 #endif
 
+#define mnprint(fmt, ...) printk(fmt " DEBUG: Passed %s %d\n", ##__VA_ARGS__, __func__, __LINE__)
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int __init hello_init(void)
 {
 #ifdef USE_DUMP_STACK
-	printk(KERN_ALERT "### [dump_stack] start ###\n");
+	//printk(KERN_ALERT "##mn: passed %s %d \n", __FUNCTION__, __LINE__);
+	printk(KERN_ALERT "###[%s %d] [dump_stack start]\n", __func__, __LINE__);
 	dump_stack();
-	printk(KERN_ALERT "### [dump_stack] over ###\n");
+	printk(KERN_ALERT "###[%s %d] [dump_stack end]\n", __func__, __LINE__);
 #endif
 
 	return 0;
@@ -23,7 +26,8 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "### test module\n");
+	//printk(KERN_ALERT "### test module exit...\n");
+	mnprint(KERN_ALERT "### test module exit...");
 }
 
 module_init(hello_init);
