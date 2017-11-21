7a74955fe94c8b9712ab99ee85e6c22afa5d9b8c
nma1x <ningx.ma@intel.com>
2017-11-21 15:03:25 +0800

    [open_src] angr

--- open_src/angr/angr-utils.sh
+++ open_src/angr/angr-utils.sh
@@ -27,9 +27,19 @@ git clone https://github.com/angr/simuvex
 cd $angr_dir/simuvex
 sudo python setup.py install
 
-#2. Test angr-utils
+#(5)install angr-utils
 cd $angr_dir
 git clone https://github.com/axt/angr-utils
+cd $angr_dir/angr-utils
+sudo python setup.py install
+
+
+#(6)angr-utils/examples/plot_cfg_region[master]$ python plot_cfg_region_example.py 
+#ImportError: cannot import name arm:
+pip install -I --no-use-wheel capstone
+
+
+#2. Test angr-utils
 cd $angr_dir/angr-utils/examples/plot_func_graph
 python plot_func_graph_example.py
 
