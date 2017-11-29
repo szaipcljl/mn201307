212a31bf0ef96eff544179b1e2ede87bba9b9b76
nma1x <ningx.ma@intel.com>
2017-11-29 16:36:44 +0800

    [scripts] shell

--- scripts/shell/search_symbols.sh
+++ scripts/shell/search_symbols.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
 # search symbols from disassembly file
+rm output -rf
+mkdir output
+
 #target=./kf4abl.asm
 target=./search_symbols_testfile
+keywords=( "mmc" "emmc" "sd" "flash" "oeminfo" "offset" "index" "kernel" "ramdisk")
+direction=("read" "write")
 
-# search seed
-grep "emmc" $target | grep "read" | grep ":$" | sed 's/>:$//' | sed 's/<//' |\
-	awk 'BEGIN{OFS=FS=":"}{split($1,A,/ /);$1=A[2]" "A[1];print $0}' |\
-	sed 's/ /, 0x/g' | tee taint_emmc_read.txt
-grep "mmc" $target | grep "read" | grep ":$" | sed '/emmc/d' | sed 's/>:$//' | sed 's/<//' |\
-	awk 'BEGIN{OFS=FS=":"}{split($1,A,/ /);$1=A[2]" "A[1];print $0}' |\
-	sed 's/ /, 0x/g' | tee taint_mmc_read.txt
+echo "source_keywords: "${source_keywords[*]}
+echo "sink_keywords: "${sink_keywords[*]}
 
-# search sink
-grep "emmc" $target | grep "write" | grep ":$" | sed 's/>:$//' | sed 's/<//' |\
-	awk 'BEGIN{OFS=FS=":"}{split($1,A,/ /);$1=A[2]" "A[1];print $0}' |\
-	sed 's/ /, 0x/g' | tee taint_emmc_write.txt
-grep "mmc" $target | grep "write" | grep ":$" | sed '/emmc/d' |\
-	sed 's/ /, 0x/g' | sed 's/>:$//' | sed 's/<//' |\
-	awk 'BEGIN{OFS=FS=":"}{split($1,A,/ /);$1=A[2]" "A[1];print $0}' |\
-	sed 's/ /, 0x/g' | tee taint_mmc_write.txt
-grep "flash" $target | grep "write" | grep ":$" | sed 's/>:$//' | sed 's/<//' |\
-	awk 'BEGIN{OFS=FS=":"}{split($1,A,/ /);$1=A[2]" "A[1];print $0}' |\
-	sed 's/ /, 0x/g' | tee taint_flash_write.txt
+# find symbol's defined line, and exchange symbol and address in the format:
+# symbol, address
+# $1: keyword
+# $2: "read" or "write"
+search_exchan_and_save()
+{
+	grep $1 $target | grep $2 | grep ":$" | sed 's/>:$//' | sed 's/<//' |\
+		awk 'BEGIN{OFS=FS=":"}{split($a,A,/ /);$a=A[2]" "A[1];print $0}' |\
+		sed 's/ /, 0x/g' >> ./output/taint_$2.txt
+}
+
+# search seeds and sinks
+echo
+echo "###### search seeds and sinks #######"
+
+for direct in ${direction[*]}
+do
+	for keywd in ${keywords[*]}
+	do
+		echo "=> keyword[$direct]: "$keywd
+		search_exchan_and_save $keywd $direct
+	done
+done
+
+echo
+echo "###### remove duplicate in taint_read.txt ######"
+awk '!x[$0]++' ./output/taint_read.txt | tee ./output/taint_source.txt
+echo
+echo "###### remove duplicate in taint_write.txt ######"
+awk '!x[$0]++' ./output/taint_write.txt | tee ./output/taint_sink.txt
+
+rm ./output/taint_read.txt ./output/taint_write.txt
