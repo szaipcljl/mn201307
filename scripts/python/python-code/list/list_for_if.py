f10ef387ba1ab9af230e6179e732c39d78454e7f
nma1x <ningx.ma@intel.com>
2017-12-22 15:32:00 +0800

    [python] website

--- scripts/python/python-code/list/list_for_if.py
+++ scripts/python/python-code/list/list_for_if.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+print "---1---"
 myList=[[0,1],[2,3],[1,5],[1,4]]
 myArray = [s for s in myList if s[0] == 1]
 print myArray
 
+print "---2---"
+myArray = [s for s in myList if s[0] == 9]
+print myArray
+if not myArray:
+    print "no items"
