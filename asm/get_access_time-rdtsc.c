634e50d4c62c20ac0e90150cf746ec08f4dcd08a
nma1x <ningx.ma@intel.com>
2018-01-30 11:22:57 +0800

    pci hacker

--- asm/get_access_time-rdtsc.c
+++ asm/get_access_time-rdtsc.c
@@ -30,10 +30,39 @@ static inline int get_access_time(volatile char *addr)
 	return cyc_low2 - cyc_low1;
 }
 
+unsigned long timed_load_loadme(volatile char *addr) {
+	long res, dummy;
+	asm volatile (
+			"mov $0, %%rax\n\t"
+			"cpuid\n\t"
+
+			"rdtsc\n\t"	//(1) read first time; time stored in %%rax
+			"mov %%rax, %0\n\t" //store time in %0(res)
+
+			"mov $0, %%rax\n\t"
+			"cpuid\n\t"
+
+			"mov (%2), %1\n\t"	//(2) load addr
+
+			"mov $0, %%rax\n\t"
+			"cpuid\n\t"
+
+			"rdtsc\n\t" //(3) read again; 
+			"sub %%rax, %0\n\t"	//%0 = %0 - %%rax
+
+			"mov $0, %%rax\n\t"
+			"cpuid\n\t"
+			: "=&r"(res), "=&r"(dummy) /* outputs */
+			: "r"(addr) /* inputs */
+			: "memory", "cc", "ax", "bx", "cx", "dx" /* clobber */);
+	//%0 -> res; %1 -> dummy; %2 -> addr
+	return -res;
+}
+
 
 int main(int argc, const char *argv[])
 {
-	int time;
+	int time, load_time;
 	volatile char *addr;
 
 	memset(target_array, 1, sizeof(target_array));
@@ -41,5 +70,8 @@ int main(int argc, const char *argv[])
 
 	time = get_access_time(addr);
 	printf("time = %d\n", time);
+
+	load_time = timed_load_loadme(addr);
+	printf("load_time = %d, load_time/time = %d\n", load_time, load_time/time);
 	return 0;
 }
