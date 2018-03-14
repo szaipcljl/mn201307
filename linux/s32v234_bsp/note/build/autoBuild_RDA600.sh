7ee5959ac9d0e82d78040a371d6335ac8992b4be
ning.ma <ning.ma@roadefend.co>
2018-03-14 17:14:21 +0800

    20180314

---
+++ linux/s32v234_bsp/note/build/autoBuild_RDA600.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "auto build IDA600"
+
+# Download the wuji's tools
+root_folder=$PWD
+echo "$root_folder"
+
+#git clone git@121.196.213.107:wuji/tools.git
+cd tools/engineering 
+./install $root_folder
+
+# Download the RDA600 & wuji source code
+cd $root_folder
+
+repo init -u git@121.196.213.107:RDA600/manifest.git
+repo sync
+
+# Build the RDA600 project
+source build/env/aarch64-linaro-gcc.env
+
+cd boards/rda600
+make sync_build_mkcfg
+make rda600_config
+make defconfig_public_linux
+make menuconfig_public_linux
+
+make -j$(nproc)
+
