dde34c6a005f83ee54d6ce28f400cb58ec6397b9
ning.ma <ning.ma@roadefend.com>
2018-07-06 16:48:04 +0800

    pinctrl
    Signed-off-by: ning.ma <ning.ma@roadefend.com>

--- linux/s32v234_bsp/note/linux-4.1.26/vision_subsystem_modules/ch66_Image-Signal-Processor_ISP/kernel/ds90ub914a/ds90ub914.h
+++ linux/s32v234_bsp/note/linux-4.1.26/vision_subsystem_modules/ch66_Image-Signal-Processor_ISP/kernel/ds90ub914a/ds90ub914.h
@@ -1,6 +1,11 @@
 #ifndef __DS90UB914_H__
 #define __DS90UB914_H__
 
+// ds90ub914 control registers
+#define DS914_SER_ALIAS 0X07
+#define DS914_SLAVE_ID1 0X09
+#define DS914_SLAVE_ALIAS1 0x11
+
 struct ds90ub914a_platform_data {
 	int ds90ub914_pwn_gpio;
 	//we see ds90ub914a and remote camera power on pin as ds90ub914a
