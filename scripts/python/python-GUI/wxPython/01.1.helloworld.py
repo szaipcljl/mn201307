97bd7d452441f222d2bf068281e3cb68bd46a0c4
ning.ma <ning.ma@roadefend.com>
2018-08-13 12:00:02 +0800

    python

---
+++ scripts/python/python-GUI/wxPython/01.1.helloworld.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+# What, you think 5 lines is too many? Okay, fine. Here it is in one line 😛 :
+import wx; a=wx.App(); wx.Frame(None, title="Hello World").Show(); a.MainLoop()
