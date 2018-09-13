0dfcc9c81d1c255c69217a4fe0477c414c00ae5d
ning.ma <ning.ma@roadefend.com>
2018-09-13 19:26:09 +0800

    ambarella build
    Signed-off-by: ning.ma <ning.ma@roadefend.com>

---
+++ scripts/python/test/os_join_path.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+
+top_dir = '/home/chris/'
+cpu_arch = 'mn201307 '
+pkg_dir = os.path.join(top_dir, 'mn', cpu_arch)
+print pkg_dir
+
+
+pkg_dir = os.path.join(top_dir, 'mn')
+print pkg_dir
