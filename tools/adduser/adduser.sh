eb3f2de7e70e47ce18cc2d7beab257869369def9
ning.ma <ning.ma@roadefend.com>
2018-09-18 17:59:32 +0800

    note

--- tools/adduser/adduser.sh
+++ tools/adduser/adduser.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #adduser username会在/home下建立一个文件夹username
 adduser $USR
+
+# add sudoers
+sudo vi /etc/sudoers
+userlist_deny=NO
+userlist_enable=YES 
+userlist_file=/etc/allowed_users
+seccomp_sandbox=NO
+local_enable=YES
+write_enable=YES
