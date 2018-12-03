6e246d3970c5ce513edac2873ebe2f2146dca62d
ning.ma <ning.ma@roadefend.com>
2018-12-03 14:57:02 +0800

    update

--- linux/arm/dms/ambarella/s5l/video/ds90ub954/954_cmd.sh
+++ linux/arm/dms/ambarella/s5l/video/ds90ub954/954_cmd.sh
@@ -24,19 +24,22 @@ export REMOTE_SER_ADDR_8BIT=0xBA
 # ar0144 slave addr: 7'bit: 0x10
 export AR0144_ADDR_8BIT=0x20
 
-#
-# ds90ub914 power down
-#
-echo 8 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio8/direction
-echo 1 > /sys/class/gpio/gpio8/value
+if [ $1x = "1"x ];then
+	echo "ds90ub954 power down and camera module power on"
+	#
+	# ds90ub914 power down
+	#
+	echo 8 > /sys/class/gpio/export
+	echo out > /sys/class/gpio/gpio8/direction
+	echo 1 > /sys/class/gpio/gpio8/value
 
-#
-# camera module(xc7027+ar0144) power on enable pin
-#
-echo 0 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio0/direction
-echo 1 > /sys/class/gpio/gpio0/value
+	#
+	# camera module(ar0144) power on enable pin
+	#
+	echo 0 > /sys/class/gpio/export
+	echo out > /sys/class/gpio/gpio0/direction
+	echo 1 > /sys/class/gpio/gpio0/value
+fi
 
 #
 # config 954 deserializer regs
