a490cf1309d31802d688ddccea5cdd8f737ae9e0
ning.ma <ning.ma@roadefend.com>
2019-01-04 17:42:10 +0800

    update

--- linux/arm/dms/ambarella/s5l/video/debug/usbnet_setup.sh
+++ linux/arm/dms/ambarella/s5l/video/debug/usbnet_setup.sh
@@ -3,5 +3,10 @@ ip_addr=192.168.2.8
 
 usbnet_name=$(ifconfig | grep enp | cut -d : -f 1 | grep "u")
 echo "#mn-usbnet_name:" ${usbnet_name}
-sudo ifconfig ${usbnet_name} ${ip_addr}
-ifconfig | grep ${ip_addr}
+
+for((;;))
+do
+	sudo ifconfig ${usbnet_name} ${ip_addr} > /dev/null
+	ifconfig | grep ${ip_addr} > /dev/null
+	sleep 2
+done
